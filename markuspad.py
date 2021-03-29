d9047e87112efd1848425216b4ee99de78a3079d
Markus Amano <markus.a.amano@gmail.com>
2021-03-28 22:31:48 -0500

    added title to templating and configs to app, basic login is avaiable.

--- markuspad.py
+++ markuspad.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template
+from flask import (Flask, render_template, session, 
+                   request, flash, redirect, url_for)
+
+DATABASE = './blog.db'
+SECRET_KEY = 'sample key'
+USERNAME = 'user'
+PASSWORD = 'bananacar'
+DEBUG = True
+
+# Create App
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 # This is the home-page
 @app.route('/')
@@ -20,6 +30,21 @@ def blog_page():
 @app.route('/about')
 def about_page():
    return render_template('about.html')
+
+#Session - (Inspired by https://github.com/QuadPiece/flask-blog/blob/master/exec.py)
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+  error = None
+  if request.method == 'POST':
+    if request.form['username'] != app.config['USERNAME']:
+      error = 'Invalid username'
+    elif request.form['password'] != app.config['PASSWORD']:
+      error = 'Invalid password'
+    else:
+      session['logged_in'] = True
+      flash('You were logged in')
+      return redirect(url_for('index'))
+  return render_template('login.html', error=error)
 
 # this is the main fucntion. 
 # Make sure to set debug to False in production.
